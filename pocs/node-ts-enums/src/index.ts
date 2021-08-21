291a03276d472983ac260eab71d7f880af92bfc1
diegopacheco <diego.pacheco.it@gmail.com>
2021-08-21 16:01:18 -0300

    added typescript to enums

---
+++ pocs/node-ts-enums/src/index.ts
@@ -0,0 +1,11 @@
+enum Direction {
+  Up = 1,
+  Down = "Down",
+  Left = "Left",
+  Right = Down
+}
+
+console.log("The only way is to go: " + Direction.Up);
+console.log("The only way is to go: " + Direction.Down);
+console.log("The only way is to go: " + Direction.Left);
+console.log("The only way is to go: " + Direction.Right);
