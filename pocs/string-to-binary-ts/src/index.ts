cbc17adb1c4c9876dfae4a2512c6c92eed8262ce
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-13 21:55:03 -0700

    string to binary string

--- pocs/string-to-binary-ts/src/index.ts
+++ pocs/string-to-binary-ts/src/index.ts
@@ -1,9 +1,11 @@
 const str = 'Hello World';
-function textToBinary(str: string): string {
+function textToBinary(str:string): string {
    let res:string = "";
    res = str.split('').map(char => {
       return char.charCodeAt(0).toString(2);
    }).join(' ');
    return res;
 };
-console.log(textToBinary('Hello World'));
+console.log("bat " + textToBinary('bat'));
+console.log("cat " + textToBinary('cat'));
+console.log("tac " + textToBinary('tac'));
