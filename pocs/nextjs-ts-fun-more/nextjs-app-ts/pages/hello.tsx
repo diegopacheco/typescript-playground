499748c2f3b7f6f6683e29ea39cbc8d879337046
diegopacheco <diego.pacheco.it@gmail.com>
2023-12-16 00:18:28 -0800

    added server side props

--- pocs/nextjs-ts-fun-more/nextjs-app-ts/pages/hello.tsx
+++ pocs/nextjs-ts-fun-more/nextjs-app-ts/pages/hello.tsx
@@ -1,3 +1,12 @@
-export default function Home(){
+export const getServerSideProps = function (){
+    return {
+        props: {
+            anwser: "42"
+        }
+    }    
+}
+
+export default function Home(props:any){
+    console.log(props);
     return <div>Hello World! Page render by nextjs at the server side.</div>   
 }
