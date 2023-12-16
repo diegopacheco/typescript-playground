12ccde310cb3ce3b772ac8354c65e7dedb9c15f8
diegopacheco <diego.pacheco.it@gmail.com>
2023-12-16 00:41:05 -0800

    added api

--- pocs/nextjs-ts-fun-more/nextjs-app-ts/pages/hello.tsx
+++ pocs/nextjs-ts-fun-more/nextjs-app-ts/pages/hello.tsx
@@ -1,12 +1,20 @@
-export const getServerSideProps = function (){
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+
+export const getServerSideProps = async function (){
+    const rawData = {
+        anwser: "42",
+        renderDate: new Date(),
+    };
+    const data = JSON.stringify(rawData);
     return {
         props: {
-            anwser: "42"
+            data
         }
     }    
 }
 
-export default function Home(props:any){
-    console.log(props);
+export default function Home(
+    props: InferGetServerSidePropsType<GetServerSideProps>){
+    console.log(props, props.renderDate);
     return <div>Hello World! Page render by nextjs at the server side.</div>   
 }
