cb1e89915c0973dd2921890304fceb10ae5e586d
diegopacheco <diego.pacheco.it@gmail.com>
2024-03-02 00:11:42 -0800

    High Order Functions in typescript

---
+++ pocs/ts-high-order-functions/index.ts
@@ -0,0 +1,16 @@
+type fnConsumerVoid = () => void;
+type fnConsumerStr = (a: string) => void;
+
+function printToConsole(s: string): void {
+  console.log(s);
+}
+
+function greeter(fn: fnConsumerStr): fnConsumerVoid {
+  return function () {
+    fn("Lazy: Hello, World from TS - High Order Functions Baby");
+  };
+}
+
+let lazyResult:fnConsumerVoid = greeter(printToConsole);
+console.log(lazyResult);
+console.log(lazyResult());
