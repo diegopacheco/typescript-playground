db2d5f4ff2cf0efc1fe140a1a0fda5d61f2c4e82
diegopacheco <diego.pacheco.it@gmail.com>
2024-12-23 02:20:35 -0800

    reactor

--- pocs/typescript-fp-pipe/index.ts
+++ pocs/typescript-fp-pipe/index.ts
@@ -1,12 +1,12 @@
 const pipe = <T>(...fns: Function[]) => (x: T) =>
     fns.reduce((v, f) => f(v), x);
+
+// Functions
+const double = (x: number) => x * 2;
+const square = (x: number) => x * x;
+const addOne = (x: number) => x + 1;
   
-  // Example Functions
-  const double = (x: number) => x * 2;
-  const square = (x: number) => x * x;
-  const addOne = (x: number) => x + 1;
+// Composed Function
+const composed = pipe(double, square, addOne);
   
-  // Composed Function
-  const doubleThenSquare = pipe(double, square, addOne);
-  
-  console.log(doubleThenSquare(5)); // Output: 101
+console.log(composed(5)); // Output: 101
